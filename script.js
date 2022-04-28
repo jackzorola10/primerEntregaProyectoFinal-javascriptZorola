156160a654ca233f38a3fd7f151f8233115dcfac
Jack Zorolasl <jackzorola10@gmail.com>
2022-04-27 19:56:30 -0500

    Added documentation

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
-// PENDIENTE
-// funcion de conversión de decimales a tiempo
-// validaciones
-// documentación
+// El objetivo de la entrega es mostrar conocimientos sobre variables, funciones, arrays, metodos y filtros. Los cuales creo que si estan representados.
+// El proyecto se divide en dos partes: FUNCIONES - ARRAYS  y el FLUJO DEL USUARIO.
 
-            // Tiempo = segundos         // 60 = 1
+// Concepto: Le presentamos al usuario una lista de canciones, compuesta por un array de objetos, las cuales el usuario podra filtrar y que despues ordenaremos. Ademas, hacemos que estas crucen por varias funciones para presentarlas mejor y extraer ciertos datos. Finalmente extraeremos una playlist final con un acumulado de tiempo que tomaria reproducirla por completo. 
+
+/// Array de 20 canciones, con los siguientes campos: id, name, autor, genero y tiempo
+    // Tiempo = segundos => hace falta establecer una funcion para presentarlos en formato mm:ss    #TOFIX#
 const songs = [
     {   id:1 , name: "Romantic Movies", autor: "Kay Vs the Moon", genero:"Rock", tiempo: 228   },
     {   id:2 , name: "En cuatro", autor: "Amigos Invisibles", genero:"Rock", tiempo: 240   },
@@ -27,6 +28,7 @@ const songs = [
     {   id:20 , name: "The Bidding", autor: "Tally Hall", genero:"Rock", tiempo: 192   },
 ]
 
+// Constructor de canciones, le permitiremos al usuario ingresar una nueva canción a la playlist la cual acomodaremos junto con las demas. 
 class newSong {
     constructor(id, name, autor, genero, tiempo) {
         this.id = songFilter.length + 1
@@ -37,7 +39,12 @@ class newSong {
     }
 }
 
-// Hay que cambiar algo aquí porque si de pura chance escriben la canción con minuscula, ya bailamos las calmadas. No quiero ni pensar si es numero o simbolo.
+
+
+
+// Funcion que toma la playlist y la reordena segun el orden alfabetico del nombre.
+    // Podríamos hacerla un poco mas generica y permitir que cambien de valor a ordenar, no solo name #TOFIX#
+    // Necesitamos hacer una validación porque parece ser que si el usuario ingresa un nombre que inicia con minuscula no lo ordena bien. #TOFIX# 
 function alphabetical (array) {
     array.sort((a, b) => {
         if (a.name > b.name) {
@@ -52,6 +59,7 @@ function alphabetical (array) {
 }
 
 
+// Toda esta sección, toma un array y sus distintos valores (los cuales no he logrado generalizar) y los imprime en un string que puedes llamar para colocarlo facilmente en un console.log. Esto es solo para presentarlo de una forma mas estetica al usuario.
 let printedMenu;
 const songMenu = [];
 function printingMenu (valor) {
@@ -70,23 +78,27 @@ function printingMenu (valor) {
     console.log(printedMenu)
 }
 printingMenu(songs);
-// Falta cambio de tiempo a formato de 00:00
+
 
 
 // FLUJO DEL USUARIO #####################################################
-
+// Se le presenta la playlist general al usuario.
 alert("Bienvenido a radio CH-Jack, la unica radio enfocada 100% en musica para castores.");
 alert("Por favor ayudanos a crear una playlist ideal para nuestros castorescuchas. \n" + "Esta es nuestra playlist actual: \n\n" + printedMenu)
 
+// Se le pide que escoga un genero para filtrar la playlist: 4 opciones posibles Rock, Pop, KPOP, Rap.
 let userGenre = prompt("A los castores no les gusta que mezclemos generos, podrías seleccionar un genero para ellos?\n\n" + "Ej. Rock, Pop, KPOP, Rap");
-// Falta validación de input
+// Falta validación de input #TOFIX#
 
 let songFilter = songs.filter(   (el) => el.genero.includes(userGenre));
 console.log(songFilter);
+// Falta rescribir los valores del id #TOFIX#
 
+// Solicite al usuario que me dijera un tiempo extra para añadir entre canción y canción, que despues agregaré al tiempo de duración de cada canción.
 let userTimeAdd = prompt("Los castores detestan cuando una canción termina y empieza la siguiente inmediatamente.\n\n ¿Cuantos segundos deberiamos esperar entre canción y canción?");
+// Falta validación para number
 
-
+// Use el mapping para reescribir los valores de "tiempo" y sumarle el userTimeAdd que preguntamos arriba.
 songFilter = songFilter.map( (el) => {
     return {
         id: el.id,
@@ -97,32 +109,41 @@ songFilter = songFilter.map( (el) => {
     }
 } );
 console.log(songFilter);
-// Si no agregas los demas valores los borra a la shingada
 
 printingMenu(songFilter);
 alert("¡Excelente! por ahora contamos con esta playlist:\n\n" + printedMenu);
 
+// Función que ordena alfabeticamente
 alphabetical(songFilter);
 
+// Función que imprime el array de objetos en un string facilmente imprimible.
 printingMenu(songFilter);
 alert("Ahora... se que esto parecera tonto, pero a los castores realmente les molesta cuando las canciones no van en orden alfabetico. Así que las acomodé aquí abajo en ese orden.\n\n" + printedMenu)
 
+// Hacemos la peticion al usuario como si este constase con una banda/autor ellos mismos y les pedimos datos para pasarselos al constructor al final
 let userAutor = prompt("¿Que dices? ¿Vos tenes una agrupación musical?\nMira, no digas que yo te lo dije, pero... podemos darte algo de promoción aquí ¿ves?, siempre y cuando sea " + userGenre + ", claro esta.\n\n" + "¿Como se llama vuestra agrupación?")
-
 let userSongName = prompt("¿Como se llama la canción mas pegajosa de " + userAutor + "?")
+let userTiempo = prompt("¿Cuanto dura " + userSongName + " en segundos?");
 
-let = userTiempo = prompt("¿Cuanto dura " + userSongName + " en segundos?");
-
+// Armamos la entrada en el array y la empujamos
 songFilter.push(new newSong(0, userSongName, userAutor, userGenre, userTiempo));
 
+// Volvemos a acomodar e imprimir
 alphabetical(songFilter);
 printingMenu(songFilter);
 
+// Extraemos el tiempo total del array y lo sumamos.
+    // Intenté un rato hacer esto con un reduce() pero no pude.
 let runtime = 0;
 for (let i = 0; i < songFilter.length; i++) {
         runtime += parseInt(songFilter[i].tiempo);
 }
 console.log(runtime);
 
+// Presentamos el valor final.
 alert("¡Venga, la he agregado aquí, esos castores van a amarlo! \n\n" + printedMenu + "\n\n El total runtime es de: " + runtime);
+
+// PENDIENTE
+    // funcion de conversión de decimales a tiempo
+    // validaciones
 
